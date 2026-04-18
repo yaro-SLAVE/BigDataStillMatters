f00df28f143740332b3d34661dfebc05b1d2f0c8
ElectronApps <barbwirek4@gmail.com>
2026-04-18 15:50:12 +0800

    main

--- solution.py
+++ solution.py
@@ -51,29 +51,44 @@ OUTPUT_CSV = Path('results.csv')
 INCLUDE_EXTS = {'mp4', 'jpg', 'html', 'parquet', 'doc', 'tif', 'pdf', 'docx', 'xls', 'md', 'json', 'txt', 'csv', 'rtf', 'gif', 'png'}
 #{'mp4', 'jpg', 'html', 'parquet', 'doc', 'tif', 'pdf', 'docx', 'xls', 'md', 'json', 'txt', 'csv', 'rtf', 'gif', 'png'}
 
-def analyze_file(path_to_file: str) -> Context:
+def analyze_file(path_to_file: Path) -> Context:
     text = TextExctractor.extract_text(path_to_file)
     print(text)
-    return []
+    return Context([], [])
 
-# Удалишь потом
-def scan_root(root: Path) -> List[Dict[str, object]]:
-    for dirpath, dirnames, filenames in os.walk(root):
+# class Level(StrEnum):
+#     UZ1 = "УЗ-1"
+#     UZ2 = "УЗ-2"
+#     UZ3 = "УЗ-3"
+#     UZ4 = "УЗ-4"
+
+# @dataclass
+# class RuleCategory:
+#     category: Category
+#     min_amount: int
+#     hint: bool = False
+
+# @dataclass
+# class Rule:
+#     categories: list[RuleCategory]
+#     level: Level
+
+# RULES = [
+#     {
+#         CommonCategory.NAME, SpecialCategory.HEALTH
+#     }
+# ]
+
+if __name__ == "__main__":
+    results = {}
+    for dirpath, dirnames, filenames in os.walk(ROOT_DIR):
         for name in filenames:
             p = Path(dirpath) / name
             ext = p.suffix.lower().lstrip('.')
             if ext not in INCLUDE_EXTS:
                 continue
             try:
-                res = analyze_file(p)
+                context = analyze_file(p)
             except Exception as e:
                 pass
             print(name)
-
-if __name__ == "__main__":
-    if ROOT_DIR.exists():
-        #TODO
-        scan_root(ROOT_DIR)
-        # pass
-    else:
-        print("Укажите корректный ROOT_DIR (существующая директория).")
